eb7b782173605c57e5d8347b0234182e03b3c4da
mwei <linmingwei100@gmail.com>
2024-06-05 18:14:55 +0800

    init project

---
+++ src/main/java/com/fastrun/springfastrun/SpringFastrunApplication.java
@@ -0,0 +1,13 @@
+package com.fastrun.springfastrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFastrunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFastrunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastrun/springfastrun/SpringFastrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastrun.springfastrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFastrunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
